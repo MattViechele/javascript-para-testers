d2424c324adcce48a8ea2a4fe8393b6a0f6544de
MattViecheleWindows <MattViechele@hotmail.com>
2026-01-05 20:29:18 -0300

    Início do curaso. Apresentação das váriavais, var, let, const, arrays e objetos

---
+++ variaveis.js
@@ -0,0 +1,29 @@
+// Ele pode ser alterada!
+var nome = 'Mateus' // String - Texto
+Nome = 'Matt'
+
+console.log(nome)
+
+// Ele pode ser alterado!
+let idade = 21 // Números
+idade = 22
+
+console.log(idade)
+
+/// NÃO pode ser alterado!
+const cidade = 'Limeira'
+
+var maior_idade = true // Boolean - verdadeiro ou falso
+
+const paises = ['Brasil', 'Polônia', 'Itália'] // Lista de dados
+
+let devices = {
+    nome: 'Samsung S23',
+    preco: 3000,
+    cores: {
+        cor1: 'Laranja',
+        cor2: 'Azul'
+    }
+}
+
+// Função 
