c589f47b4af36a250ab7bda9301977ef3c38bb9a
MattViecheleWindows <MattViechele@hotmail.com>
2026-01-06 19:47:05 -0300

    Apresentação a estruturas de decisão

---
+++ decisao.js
@@ -0,0 +1,33 @@
+// if - else
+
+// if (condição) {
+
+// } else {
+
+// }
+
+const nome = 'Mateus'
+
+// if (nome === 'Mateus') {
+//     console.log('Condição atendida!')
+// } else {
+//     console.log('Condição não atendida!')
+// }
+
+let idade = 22
+
+// if (idade === 30) {
+//     console.log('Condição atendida!')
+// } else if (idade === 22) {
+//     console.log('Condição atendida 1!')
+// } else {
+//     console.log('Nenhuma condição atendida!')
+// }
+
+const maiorIdade = true;
+
+if (maiorIdade) {
+    console.log('É maior de idade!')
+} else {
+    console.log('Não é maior de idade!')
+}
