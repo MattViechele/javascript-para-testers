dc4955d9d048ce350139200232a949944b3fe007
MattViecheleWindows <MattViechele@hotmail.com>
2026-01-06 20:11:58 -0300

    Apresentação das condições de repetições

---
+++ repeticao.js
@@ -0,0 +1,22 @@
+// Repetição por array
+// array.forEach(element => {
+    
+// });
+
+const cidades = ['São Paulo', 'Rio de Janeiro', 'Florianópolis', 'Porto Alegre']
+
+// cidades.forEach((elemento, index) => {
+//     console.log('Execução número: ' + index)
+//     console.log(elemento)
+// });
+
+// for normal
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let index = 0; index < cidades.length; index++) {
+    console.log(cidades[index]);
+}
