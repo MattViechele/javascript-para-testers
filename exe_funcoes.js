d6811053114ec9e9d5440bc34c838733723fcc16
MattViecheleWindows <MattViechele@hotmail.com>
2026-01-07 20:49:35 -0300

    Resolução do Exercício 3 sobre funções e término do curso

---
+++ exe_funcoes.js
@@ -0,0 +1,19 @@
+// Exercício 3 - Funções
+
+// Criar uma função 
+// Que soma 2 números e retorna esses números somado
+// Deve ser enviado por parâmetros os 2 números da conta
+
+// Número1: 20
+// Número2: 10
+// Soma: 30
+
+function soma(numero1, numero2) {
+    console.log('Numero1: ', numero1)
+    console.log('Numero2 ', numero2)
+    return numero1 + numero2
+}
+
+console.log('Soma: ', soma(21, 1))
+const resultado = soma(20, 2)
+console.log('Soma: ', resultado);
