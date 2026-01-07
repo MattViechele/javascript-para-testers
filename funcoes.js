4cb413398539decb634553ea139fe374c02cac81
MattViecheleWindows <MattViechele@hotmail.com>
2026-01-07 20:30:38 -0300

    Apresentação de funções

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+// Funções
+// Precisa chamar
+// Pode ou não receber parâmetros
+// Pode ou não retornar dados
+
+function gerarNome() {
+    const nome = 'Mateus'
+    const sobrenome = ' Viechele'
+    const idade = 22
+
+    console.log('Nome: ', nome + sobrenome)
+    console.log('Idade: ', idade)
+}
+
+gerarNome();
